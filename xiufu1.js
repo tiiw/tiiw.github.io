5a69ae972cc253bcbed75c61c5c6e5aabf79cc69
tiiw <141731239+tiiw@users.noreply.github.com>
2024-03-15 14:16:54 +0800

    Create xiufu1.js

---
+++ xiufu1.js
@@ -0,0 +1,56 @@
+var conf = {
+	"path": "0",
+	"title": [
+		"欢迎使用！"
+	],
+	"census": "1",
+	"jiami": "0",
+	"deny": "0",
+	"cache": "86400",
+	"adth1": "此资源需要分享到微信群才能解锁观看完整版！点击下方一键复制<br>“点下面复制链接-粘贴到群”<br>粘贴到微信群即可解锁完整版！！<br>完成后打开发出去的链接观看！",
+	"sinfo": "{www}各种NBA赛事！！\r\n###\r\n如果上面打不开\r\n请+微信裙→http://baidu.com#{nn}{ww}",
+	"sht": [
+		"合集"
+	],
+	"she": [
+		"已解锁全部！点击进入观赏！"
+	],
+	"shu": [
+		"?_ks={wwww}&vid=VIDEOID&TOKEN&{www}={wwnn}"
+	],
+	"tongji": "",
+	"send": "复制成功，发送到群里即可!<br>点击发送出去的链接可观看完整版",
+	"btn4": "点 这 里 进 微信 群  看",
+	"url4": [
+		"http://qq.com#{nnnnnnnn}{www}"
+	],
+	"url5": [
+		"http://www.baidu.com/"
+	],
+	"shi": [
+		"https://qqadapt.qpic.cn/txdocpic/0/ad4763e8c1bd22bbd9e04178985b1a63/0",
+		"https://qqadapt.qpic.cn/txdocpic/0/6bb0122a3c65a65e1a5505e357b561c7/0",
+		"https://qqadapt.qpic.cn/txdocpic/0/fe883cfefd416015b369379e6cc3b77f/0"
+	],
+	"videolist": [
+	{
+		"src": "https://vod.qiecdn.com/rec_m3u8/20231114/10005196rTY2PuRo-01b4956ff553badb278917255026be7b-720/playlist.m3u8",
+		"id": "85",
+		"time": "180"
+	},
+  	{
+		"src": "https://vod.qiecdn.com/rec_m3u8/20240312/10005196ruqiMu6l-5f8a7759cd4a67220a5aa864e3164031-720/playlist.m3u8",
+		"id": "85",
+		"time": "180"
+	},
+  	{
+		"src": "https://vod.qiecdn.com/m3u8/20181220/live-10005196rtfVY7Bv--20181220083209_ebbcdcd71-1080.m3u8",
+		"id": "85",
+		"time": "180"
+	},
+	],
+	"url1": [
+		""
+	],
+	"mobile": "0"
+}
